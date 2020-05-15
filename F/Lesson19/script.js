17f2cef63aa69d495b80165bf932cfccabcacd06
DedrDs <61499464+DedrDs@users.noreply.github.com>
2020-05-15 16:43:12 +0300

    Friday All Lessons

---
+++ F/Lesson19/script.js
@@ -0,0 +1,20 @@
+$("document").ready(function(){
+	$(".box6-simple-btn").click(function(){
+		let x = this.id;
+		if (x == "box6-simple-btn1"){
+			document.getElementById("box6-simple-btn1").style.background = "#ddd"; // светлый
+			document.getElementById("box6-simple-btn2").style.background = "#bbb"; // темный
+			document.getElementById("box6-simple-btn3").style.background = "#bbb"; // темный
+		}
+		else if(x == "box6-simple-btn2"){
+			document.getElementById("box6-simple-btn1").style.background = "#bbb"; // темный
+			document.getElementById("box6-simple-btn2").style.background = "#ddd"; // светлый
+			document.getElementById("box6-simple-btn3").style.background = "#bbb"; // темный
+		}
+		else if(x == "box6-simple-btn3"){
+			document.getElementById("box6-simple-btn1").style.background = "#bbb"; // темный
+			document.getElementById("box6-simple-btn2").style.background = "#bbb"; // темный
+			document.getElementById("box6-simple-btn3").style.background = "#ddd"; // светлый
+		}
+	})
+})
